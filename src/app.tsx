be568473f0e9751dd0dcb7880c91da1de0f5cbc4
fltzr <josh.salinas.j@outlook.com>
2024-08-26 01:16:07 -0400

    feat: add theme

--- src/app.tsx
+++ src/app.tsx
@@ -3,8 +3,11 @@ import { RouterProvider } from 'react-router-dom';
 
 import { router } from './router';
 import { getCookie } from './common/utils/cookies';
+import { useTheme } from './common/hooks/use-theme';
 
 export const App = () => {
+  useTheme();
+
   useEffect(() => {
     console.log(getCookie('Cookie1'));
   }, []);
---
+++ src/common/hooks/use-theme.ts
@@ -0,0 +1,16 @@
+import { applyMode, Mode } from "@cloudscape-design/global-styles"
+import { useEffect, useState } from "react"
+
+export const useTheme = () => {
+    const [theme, setTheme] = useState<Mode>(() => {
+        const savedTheme = localStorage.getItem('theme');
+        return savedTheme ? savedTheme as Mode : Mode.Dark;
+    });
+
+    useEffect(() => {
+        applyMode(theme);
+        localStorage.setItem('theme', theme);
+    }, [theme]);
+
+    return { theme, toggleTheme: () => setTheme(prev => (prev === Mode.Dark ? Mode.Light : Mode.Dark)) }
+};
