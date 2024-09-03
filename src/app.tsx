0f1964d5f53213149791de3aca4a5167e8fd9c8a
fltzr <josh.salinas.j@outlook.com>
2024-09-02 20:49:54 -0400

    Add initial popup modal and query client providers

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,49 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { RouterProvider } from 'react-router-dom';
 
 import { router } from './router';
-import { getCookie } from './common/utils/cookies';
 import { useTheme } from './common/hooks/use-theme';
+import { Box, Header, Modal, StatusIndicator } from '@cloudscape-design/components';
+import { SupabaseProvider } from './common/hooks/use-supabase';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5, // 5 minutes
+      gcTime: 1000 * 60 * 30, // 30 minutes
+    },
+  },
+});
 
 export const App = () => {
+  const [initialPopup, setInitialPopup] = useState(false);
   useTheme();
 
   useEffect(() => {
-    console.log(getCookie('Cookie1'));
+    const hasShownPopup = localStorage.getItem('shown-initial-popup') ?? false;
+
+    if (!hasShownPopup) {
+      setInitialPopup(true);
+      localStorage.setItem('shown-initial-popup', 'true');
+    }
   }, []);
 
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <SupabaseProvider>
+        <RouterProvider router={router} />
+        <Modal
+          visible={initialPopup}
+          onDismiss={() => setInitialPopup(false)}
+          header={<Header variant='h1'>Coucou beautiful bABY</Header>}>
+          I{' '}
+          <Box variant='span'>
+            <StatusIndicator type='success'>LOVE</StatusIndicator>
+          </Box>{' '}
+          you!!!!!!!!
+        </Modal>
+      </SupabaseProvider>
+    </QueryClientProvider>
+  );
 };
