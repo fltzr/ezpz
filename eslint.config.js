b83c0c27a54526d29de39cc226e067dc20c83ee7
fltzr <josh.salinas.j@outlook.com>
2024-09-08 17:41:18 -0400

    Apply additional react eslint configurations

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,9 @@ import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 import pluginReact from 'eslint-plugin-react';
+import pluginReactCompiler from 'eslint-plugin-react-compiler';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
+import pluginReactRefresh from 'eslint-plugin-react-refresh';
 
 export default tseslint.config(
   eslint.configs.recommended,
@@ -18,10 +21,17 @@ export default tseslint.config(
     },
     plugins: {
       react: fixupPluginRules(pluginReact),
+      'react-compiler': pluginReactCompiler,
+      'react-hooks': fixupPluginRules(pluginReactHooks),
+      'react-refresh': fixupPluginRules(pluginReactRefresh),
     },
     rules: {
       ...pluginReact.configs.recommended.rules,
       ...pluginReact.configs['jsx-runtime'].rules,
+      'react-compiler/react-compiler': 'error',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'error',
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     },
     settings: {
       react: {
