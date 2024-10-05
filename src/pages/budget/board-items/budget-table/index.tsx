eea9f7f50c0eea7fa3ccb41a6783846bb7726f34
fltzr <josh.salinas.j@outlook.com>
2024-10-05 15:51:38 -0400

    feat: add button to add budget item on table header

---
+++ src/pages/budget/board-items/budget-table/index.tsx
@@ -0,0 +1,15 @@
+import { WidgetConfig } from '../../utils/widget-types';
+import i18n from '../../../../i18n';
+import { BudgetTableActions } from './actions';
+import { BudgetTable } from './table';
+
+export const budgetTableWidget: WidgetConfig = {
+  columnOffset: { 4: 0 },
+  definition: { defaultRowSpan: 3, defaultColumnSpan: 4 },
+  data: {
+    title: i18n.t('budget.budgetTable.title'),
+    description: 'Budget description',
+    content: <BudgetTable />,
+    actions: <BudgetTableActions />,
+  },
+};
--- src/pages/budget/board-items/budget-table/table.tsx
+++ src/pages/budget/board-items/budget-table/table.tsx
@@ -1,76 +1,22 @@
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { DateTime } from 'luxon';
 
-import {
-  Button,
-  SpaceBetween,
-  StatusIndicator,
-  Table,
-} from '@cloudscape-design/components';
+import { StatusIndicator, Table } from '@cloudscape-design/components';
 import { useCollection } from '@cloudscape-design/collection-hooks';
 
 import { useDrawer } from '@/components/drawer-provider';
-import { ManualRefresh } from '@/components/manual-refresh';
 
-import { useBudgetProvider } from '../hooks/use-budget-provider';
-import { useBudgetApi } from '../hooks/use-budget-api';
+import { useBudgetProvider } from '../../hooks/use-budget-provider';
+import { useBudgetApi } from '../../hooks/use-budget-api';
+import { AddBudgetItem } from '../../drawer/add-budget-item';
+import { DeleteItemModal } from '../../modals/delete-item';
+import { EditBudgetItem } from '../../drawer/edit-budget-item';
+import { EditCategory } from '../../drawer/edit-category';
 
-import { AddCategory } from '../drawer/add-category';
-import { AddBudgetItem } from '../drawer/add-budget-item';
-import { DeleteItemModal } from '../modals/delete-item';
-import { EditBudgetItem } from '../drawer/edit-budget-item';
-import { EditCategory } from '../drawer/edit-category';
+import { type BudgetTableItem, isCategoryItem } from '../../utils/api-types';
+import { createBudgetTableColumnDefinitions } from '../../utils/table-configs';
 
-import { type BudgetTableItem, isCategoryItem } from '../utils/api-types';
-import { createBudgetTableColumnDefinitions } from '../utils/table-configs';
-import { WidgetConfig } from '../utils/widget-types';
-import i18n from '../../../i18n';
-
-const BudgetTableActions = () => {
-  const { t } = useTranslation(undefined, { keyPrefix: 'budget.budgetTable' });
-
-  const { openDrawer, closeDrawer } = useDrawer();
-  const { selectedUser, budgetEntry } = useBudgetProvider();
-  const { isFetching, dataUpdatedAt, refetch, handleAddCategory } = useBudgetApi(
-    selectedUser.userId,
-    budgetEntry
-  );
-  const [lastRefresh, setLastRefresh] = useState<string | null>(null);
-
-  const onClickAddCategoryDrawer = () => {
-    openDrawer({
-      drawerName: 'add-category',
-      width: 350,
-      content: (
-        <AddCategory
-          selectedUserId={selectedUser.userId}
-          onAdd={handleAddCategory}
-          onClose={closeDrawer}
-        />
-      ),
-    });
-  };
-
-  return (
-    <SpaceBetween size='xs' direction='horizontal'>
-      <ManualRefresh
-        lastRefresh={lastRefresh}
-        loading={isFetching}
-        onRefresh={() => {
-          setLastRefresh(DateTime.fromMillis(dataUpdatedAt).toISO());
-          refetch().catch(console.error);
-        }}
-      />
-      <Button variant='primary' iconName='add-plus' onClick={onClickAddCategoryDrawer}>
-        {t('addCategory')}
-      </Button>
-    </SpaceBetween>
-  );
-};
-
-// eslint-disable-next-line react-refresh/only-export-components
-const BudgetTable = () => {
+export const BudgetTable = () => {
   const { t } = useTranslation(undefined, { keyPrefix: 'budget.budgetTable' });
   const { openDrawer, closeDrawer } = useDrawer();
   const { selectedUser, budgetEntry } = useBudgetProvider();
@@ -141,6 +87,7 @@ const BudgetTable = () => {
               width: 300,
               content: (
                 <AddBudgetItem
+                  budgetEntry={budgetEntry}
                   selectedUserId={selectedUser.userId}
                   categoryId={item.id}
                   onAdd={handleAddBudgetItem}
@@ -184,15 +131,4 @@ const BudgetTable = () => {
       />
     </>
   );
-};
-
-export const budgetTableWidget: WidgetConfig = {
-  columnOffset: { 4: 0 },
-  definition: { defaultRowSpan: 3, defaultColumnSpan: 4 },
-  data: {
-    title: i18n.t('budget.budgetTable.title'),
-    description: 'Budget description',
-    content: <BudgetTable />,
-    actions: <BudgetTableActions />,
-  },
 };
