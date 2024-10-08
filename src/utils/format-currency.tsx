82b8c4d82b9cb3f8b74686cc725eea5db5849d44
fltzr <josh.salinas.j@outlook.com>
2024-10-07 23:15:49 -0400

    chore: simplify getting user locale logic

--- src/utils/format-currency.tsx
+++ src/utils/format-currency.tsx
@@ -1,7 +1,7 @@
 import getUserLocale from 'get-user-locale';
 
 export const formatCurrency = (amount?: number) => {
-  const currency = getUserLocale() === 'fr-FR' ? 'EUR' : 'USD';
+  const currency = getUserLocale().includes('fr') ? 'EUR' : 'USD';
   if (amount === undefined) return '-';
 
   return new Intl.NumberFormat(getUserLocale(), {
