a25d411d28ab60bc28ef72ccf29807618ddfa51b
fltzr <josh.salinas.j@outlook.com>
2024-11-02 21:01:59 -0400

    perf: change vite config to use swc

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,20 @@
 /// <reference types="vitest/config" />
 
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import { defineConfig } from 'vite';
+import babel from 'vite-plugin-babel';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
+    react(),
+    babel({
+      filter: /\.[jt]sx?$/,
+      babelConfig: {
+        presets: ['@babel/preset-typescript'],
+        plugins: ['babel-plugin-react-compiler'],
       },
     }),
     viteTsconfigPaths(),
